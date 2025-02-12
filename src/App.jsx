376f648b1de982a85084f92faa43f742135767e3
eugide <eugidendungutse770@gmail.com>
2025-02-12 10:45:20 +0200

    chore: setup add my changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 function App() {
   return (
     <>
-    
+      <section className="w-11/12 m-auto my-10">
+        <div className="grid gap-8 md:grid-cols-2 md:text-center md:text-left">
+          <div>
+            <img
+              className="w-full rounded-lg "
+              src="/image/deseur.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h1 className="text-4xl text-center font-medium mb-2">Headline</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+              minima aspernatur modi. Nobis odit doloribus unde asperiores ipsa
+              voluptates quidem magnam, nihil sapiente accusamus. Ducimus
+              expedita totam atque, iste porro aperiam beatae est repudiandae
+              numquam perspiciatis adipisci delectus amet in quia corrupti
+              sapiente harum dolorem dolor optio, tempora debitis rerum impedit
+              laboriosam fugiat. Necessitatibus dicta omnis ad suscipit, eos
+              alias, harum amet beatae maiores eum quam laboriosam aut
+              aspernatur doloribus. Aliquam dicta praesentium nostrum saepe,
+              dolore pariatur nam. Rem placeat sint quidem ducimus expedita,
+              enim, at nulla, porro nesciunt accusamus itaque aspernatur
+              corrupti nihil tempore doloribus ipsa modi cumque totam?
+            </p>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
